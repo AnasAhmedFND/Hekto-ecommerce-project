d0ef62e92427c26f6eb42214561480c7bc2df6ac
AnasAhmedFND <anasahmed4402@gmail.com>
2024-11-27 11:50:35 +0600

    padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    
+    container:{
+      padding:'10px'
+    },
     extend: {
       backgroundColor:{
         'biguni':'#FB2E86'
