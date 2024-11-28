9b58f5152ea7ba9516ece4ddcd85ed2d862c6d4e
AnasAhmedFND <anasahmed4402@gmail.com>
2024-11-28 09:40:13 +0600

    respons this item of price

--- src/Components/ProductShop.jsx
+++ src/Components/ProductShop.jsx
@@ -8,21 +8,28 @@ const ProductShop = () => {
     
    let [category, setCategory] = useState([])
    let [brand, setBrand] = useState([])
+   let [price, setPrice] = useState([])
+
 
 
    useEffect(() => {
         setCategory([...new Set(shopApiProduct.map((item)=> item.category))])
         setBrand([...new Set(shopApiProduct.map((item)=> item.brand))])
+        setPrice([...new Set(shopApiProduct.map((item)=> item.price))])
+
 
    }, [shopApiProduct])
 
   
    let [categoryShow, setCategoryShow] = useState(false)
    let [brandShow, setBrandShow] = useState(false)
+   let [priceShow, setPriceShow] = useState(false)
 
 
      let [categoryItem, setCategoryItem] = useState([])
      let [brandItem, setBrandItem] = useState([])
+     let [priceItem, setPriceItem] = useState([])
+
 
 
     const hendleCategory = (c) => {
@@ -38,8 +45,8 @@ const ProductShop = () => {
     
   return (
     <>
-      <div className="container mx-auto md:flex justify-between py-10">
-        <div className="md:w-[18%]  w-[60%]">
+      <div className="container mx-auto md:flex justify-between py-10   ">
+        <div className="md:w-[18%]  w-[60%]  " >
 
           <div className="  ">
               <div className=" ">
@@ -49,13 +56,13 @@ const ProductShop = () => {
               {categoryShow &&
                   <ul className='overflow-y-scroll h-36'>
                   {category.map((item) =>(
-                    <li className='cursor-pointer md:bg-white bg-[#30e0dd]  ' onClick={() => hendleCategory (item)}>{item}</li>
+                    <li className='cursor-pointer md:bg-white bg-[#30e0dd] border mb-2 pl-2 py-1 shadow-md ' onClick={() => hendleCategory (item)}>{item}</li>
                   ))}          
             </ul>
               }
           </div>
-          
-          <div className="  ">
+
+          <div className="mt-2  ">
               <div className=" ">
                   <h1 onClick={() => setBrandShow(!brandShow)} className='flex items-center font-bold cursor-pointer'>Shop By Brand <span className='ml-5'><TiArrowSortedDown /></span></h1>
                   
@@ -63,7 +70,21 @@ const ProductShop = () => {
               {brandShow &&
                   <ul className='overflow-y-scroll h-36'>
                   {brand.map((item) =>(
-                    <li className='cursor-pointer md:bg-white bg-[#30e0dd]  ' onClick={() => hendleCategory (item)}>{item}</li>
+                    <li className='cursor-pointer md:bg-white bg-[#30e0dd] border mb-2 pl-2 py-1 shadow-md  ' onClick={() => hendleCategory (item)}>{item}</li>
+                  ))}          
+            </ul>
+              }
+          </div>
+
+          <div className="mt-2  ">
+              <div className=" ">
+                  <h1 onClick={() => setPriceShow(!priceShow)} className='flex items-center font-bold cursor-pointer'>Shop By Price <span className='ml-5'><TiArrowSortedDown /></span></h1>
+                  
+              </div>
+              {priceShow &&
+                  <ul className='overflow-y-scroll h-36'>
+                  {price.map((item) =>(
+                    <li className='cursor-pointer md:bg-white bg-[#30e0dd] border mb-2 pl-2 py-1 shadow-md  ' onClick={() => hendleCategory (item)}>{item}</li>
                   ))}          
             </ul>
               }
@@ -78,7 +99,7 @@ const ProductShop = () => {
             {categoryItem.length == 0 ? 
             shopApiProduct.map((item) => (
               
-               <div className="border md:w-[250px] w-[150px] h-[363px] text-center shadow-xl group ">
+               <div className="border md:w-[250px] w-[150px] md:h-[363px] h-[280px] text-center shadow-xl group ">
                  <img className='border bg-[#f5f5f8] group-hover:bg-[#bad9c899] ' src={item.thumbnail} alt="" />
                
                  <h4 className='font-bold'>{item.title} </h4>
