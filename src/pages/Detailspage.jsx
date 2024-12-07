445bc5be7cb7cdea9a86fc744cdfde3163bf94ce
AnasAhmedFND <anasahmed4402@gmail.com>
2024-12-07 09:58:02 +0600

    details in description

--- src/pages/Detailspage.jsx
+++ src/pages/Detailspage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ShopGrid from '../Components/ShopGrid'
 import Details from '../Components/shopPages/Details'
+import Description from '../Components/shopPages/Description'
 
 const Detailspage = () => {
   return (
     <>
       <ShopGrid pageName='Product Details' />
       <Details/>
+      <Description />
     </>
   )
 }
