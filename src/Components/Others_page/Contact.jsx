4dff4a160b9199c2cc09bfd6975fc760794fc622
AnasAhmedFND <anasahmed4402@gmail.com>
2024-12-14 14:58:24 +0600

    contact get in touch section done design

--- src/Components/Others_page/Contact.jsx
+++ src/Components/Others_page/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Cont from '../img/contact_ph.png'
 
 
 const Contact = () => {
@@ -8,7 +9,7 @@ const Contact = () => {
                 <div className="flex">
                     <div className=" w-1/2 py-5 px-2">
                         <h2 className='font-bold text-2xl font-josefin text-orgain  '>Information About Us</h2>
-                        <p className='text-[12px] '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis neque ultrices <br /> mattis aliquam, malesuada diam est. Malesuada sem tristique amet erat vitae <br /> eget dolor lobortis. Accumsan faucibus vitae lobortis quis bibendum quam.</p>
+                        <p className='text-[14px] pt-2 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis neque ultrices <br /> mattis aliquam, malesuada diam est. Malesuada sem tristique amet erat vitae <br /> eget dolor lobortis. Accumsan faucibus vitae lobortis quis bibendum quam.</p>
                         <div className="flex gap-3 mt-5">
                         <p className='w-5 h-5 bg-blue-700 rounded-full'></p>
                         <p className='w-5 h-5 bg-[#fb24ba] rounded-full'></p>
@@ -42,23 +43,23 @@ const Contact = () => {
                     </div>
                 </div>
 
-                <div className="">
-                    <div className="">
-                        <h2>Get In Touch</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis neque ultrices <br /> tristique amet erat vitae eget dolor los vitae lobortis quis bibendum quam.</p>
-                        <div className="">
-                            <div className="">
-                                <input type="text" placeholder='Your Name*' />
-                                <input type="text" placeholder='Your E-mail' />
+                <div className="pt-20 pb-10 flex">
+                    <div className=" w-1/2">
+                        <h2 className='font-bold text-2xl font-josefin text-orgain '>Get In Touch</h2>
+                        <p className='text-[14px] mt-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis neque ultrices <br /> tristique amet erat vitae eget dolor los vitae lobortis quis bibendum quam.</p>
+                        <div className=" mt-5">
+                            <div className="flex justify-between">
+                                <input className='border-2 w-[48%] py-2 pl-2 outline-none rounded-md  ' type="text" placeholder='Your Name*' />
+                                <input className='border-2 w-[48%] py-2 pl-2 outline-none rounded-md  ' type="text" placeholder='Your E-mail' />
 
                             </div>
-                            <input type="text" placeholder='Subject*' />
-                            <textarea name="" id="" placeholder='Text Your Massege*' ></textarea>
-                            <button>Send Mail</button>
+                            <input className='border-2 w-full py-2 pl-2 mt-8 outline-none rounded-md  ' type="text" placeholder='Subject*' />
+                            <textarea className='border-2 mt-8 w-full pl-2 outline-none rounded-md' name="text" id="#" placeholder='Text Your Massege*' ></textarea>
+                            <button className='borded bg-biguni py-2 px-5 text-white rounded-md mt-5'>Send Mail</button>
                         </div>
                     </div>
-                    <div className="">
-                        <img src="" alt="" />
+                    <div className="w-1/2 ">
+                        <img src={Cont} alt="" />
                     </div>
                 </div>
             </div>
